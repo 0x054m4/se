cfe6f12bf1686ef39f5d6708e5c81d972c64110a
Omar Hashad <omarhashad88@gmail.com>
2025-04-15 00:50:38 +0200

    Update Student.java

--- UniversityLibraryManagementSystem/src/Student.java
+++ UniversityLibraryManagementSystem/src/Student.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 
 public class Student extends User{
-    private ArrayList<BorrowHistory> borrowHistory;
+    private ArrayList<Borrow> borrowList;
+
     public Student(int userID) {
         super(userID);
-        this.borrowHistory = new ArrayList<>();
+        this.borrowList = new ArrayList<>();
     }
 public Student(String name, String email, String password, String role, Status status) {
         super(name, email, password, role, status);
-        this.borrowHistory = new ArrayList<>();
+        this.borrowList = new ArrayList<>();
     }
 
    
