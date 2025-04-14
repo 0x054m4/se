9895538e0d55f596c9879cdf2aa2c3e8301252f7
1-cret <omayr242994@bue.edu.eg>
2025-04-15 01:28:23 +0200

    Update Borrow.java

--- UniversityLibraryManagementSystem/src/Borrow.java
+++ UniversityLibraryManagementSystem/src/Borrow.java
@@ -1,8 +1,5 @@
-import java.sql.Connection;
 import java.util.Date;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.sql.SQLException;
+
 public class Borrow {
     private int borrowID;
     private Date borrowDate;
@@ -12,35 +9,9 @@ public class Borrow {
     private double maxAmount;
     private Student student;
     private Book book;
-    private String dbURL = "jdbc:derby://localhost:1527/UniversityLibraryDB;create=true";
-    private Connection connection = null;
-    private void connect() {
-        try {
-            Class.forName("org.apache.derby.jdbc.ClientDriver");
-            connection = DriverManager.getConnection(dbURL);
-            System.out.println("Connected successfully to DB.");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Derby ClientDriver not found.");
-            e.printStackTrace();
-        } catch (SQLException e) {
-            System.out.println("Failed to connect to DB.");
-            e.printStackTrace();
-        }
-    }
-    public void closeConnection() {
-        try {
-            if (connection != null && !connection.isClosed()) {
-                connection.close();
-                System.out.println("DB connection closed.");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
     
     public Borrow(int borrowID) {
         this.borrowID = borrowID;
-        connect();
     }
 
     public Borrow( Date borrowDate, Date dueDate, Book book, Student student) {
@@ -48,7 +19,6 @@ public class Borrow {
         this.dueDate = dueDate;
         this.book = book;
         this.student = student;
-        connect();
     }
     
     public int getBorrowID() {
@@ -124,38 +94,4 @@ public class Borrow {
         Date currentDate = new Date();
         return currentDate.after(dueDate);
     }
-
-    // DB operations
-    public void addBorrow() throws SQLException {
-        String sql = "INSERT INTO Borrow (borrowDate, dueDate, returnDate, status, maxAmount, studentID, bookID) VALUES ('"
-            + new java.sql.Date(borrowDate.getTime()) + "', '"
-            + new java.sql.Date(dueDate.getTime()) + "', '"
-            + (returnDate != null ? new java.sql.Date(returnDate.getTime()) : null) + "', '"
-            + status + "', "
-            + maxAmount + ", "
-            + student.getUserID() + ", "
-            + book.getISBN() + ")";
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sql);
-     
-    }
-    public void updateBorrow() throws SQLException{
-        String sql = "UPDATE Borrow SET borrowDate = '" + new java.sql.Date(borrowDate.getTime()) + "', dueDate = '"
-            + new java.sql.Date(dueDate.getTime()) + "', returnDate = '"
-            + (returnDate != null ? new java.sql.Date(returnDate.getTime()) : null) + "', status = '"
-            + status + "', maxAmount = "
-            + maxAmount + ", studentID = "
-            + student.getUserID() + ", bookID = "
-            + book.getISBN() + " WHERE borrowID = " + borrowID;
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sql);
-       
-    }
-
-    public void removeBorrow() throws SQLException{
-        String sql = "DELETE FROM Borrow WHERE borrowID = " + borrowID;
-        Statement statement = connection.createStatement();
-        statement.executeUpdate(sql);
-    }
-
 }
