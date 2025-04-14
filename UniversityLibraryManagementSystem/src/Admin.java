49d8bed751050b1ac28d15eccf1af5f58fe46781
Omar Hashad <omarhashad88@gmail.com>
2025-04-15 01:03:33 +0200

    Create Admin.java

---
+++ UniversityLibraryManagementSystem/src/Admin.java
@@ -0,0 +1,14 @@
+public class Admin extends User {
+
+    public Admin(int userID) {
+        super(userID);
+    }
+    public Admin(String name, String email, String password, String role, Status status) {
+        super(name, email, password, role, status);
+    }
+    public void generateReport() {
+        // 
+        System.out.println("");
+        
+    }
+}
